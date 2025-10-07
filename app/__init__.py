dcae0a6f3873b9227bddf464a19154f5c1cd6447
lele888huat <seahlele@gmail.com>
2025-10-08 00:52:16 +0800

    Fixed remember me feature

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, session
 from flask_mongoengine import MongoEngine
+from datetime import timedelta
 
 db = MongoEngine()  # declare globally so it can be imported in models.py later
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'you-will-never-guess'
+    #Remember me settings
+    app.config['REMEMBER_COOKIE_DURATION'] = timedelta(days=7)
+    app.config['REMEMBER_COOKIE_REFRESH_EACH_REQUEST'] = False
+
     app.config['MONGODB_SETTINGS'] = {
         'db': 'libraryDB',
         'host': 'localhost',
